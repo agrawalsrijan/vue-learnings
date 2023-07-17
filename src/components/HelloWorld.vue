b6efd281484c1814e33d399c2277cba50e9e7e4b
Srijan Kumar-BNG <112464261+srijan-kumar-BNG@users.noreply.github.com>
2023-07-17 14:23:50 +0530

    push initial changes for demo vue app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <p>Count = {{ count }}</p>
+    <p v-for="number in getArrayTillCount()" :key="number">{{ number }}</p>
+    <p>Learn more at <a v-bind:href="githubLink">agrawalsrijan</a> github.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    count: Number,
+    githubLink: String,
+  },
+  methods: {
+    increment() {
+      this.$emit('increment');
+    },
+    decrement() {
+      this.$emit('decrement');
+    },
+    getArrayTillCount: function () {
+      return Array.from({ length: this.count }, (_, index) => index + 1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+button {
+  margin: 5px;
+  width: 40px;
+  height: 40px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import {createRouter, createWebHistory} from 'vue-router';
+import App from './App.vue'
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+const appRouter = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/teams', component: TeamsList},
+        {path: '/users', component: UsersList},
+        {path: '/teams/:teamId', component: TeamMembers}
+    ]
+})
+
+const app = createApp(App)
+app.use(appRouter)
+app.mount('#app')
