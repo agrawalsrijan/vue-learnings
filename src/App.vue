f0598521c89540e188a3168e4ab0286fbe802276
Srijan Kumar-BNG <srijanag13@gmail.com>
2023-07-18 14:52:26 +0530

    add example fo shopping app with vuex

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,14 @@
 <template>
-  <TheNavigation></TheNavigation>
-  <main>
-    <router-view></router-view>
-  </main>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <script>
-import TheNavigation from './components/nav/TheNavigation.vue';
+import TheHeader from './components/nav/TheHeader.vue';
 
 export default {
   components: {
-    TheNavigation
-  },
-  data() {
-    return {
-      teams: [
-        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
-        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
-        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
-      ],
-      users: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
-        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
-        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
-        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
-      ],
-    };
-  },
-  provide() {
-    return {
-      teams: this.teams,
-      users: this.users,
-    };
-  },
-  methods: {
+    TheHeader
   },
 };
 </script>
